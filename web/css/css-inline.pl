c9b3792d246632b155355adb2d84cade5916b098
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-09-24 15:46:29 +0200

    Inline images from bower

---
+++ web/css/css-inline.pl
@@ -0,0 +1,59 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Simple script to inline all images we   get  from the bower dependencies
+into the css file.  Usage:
+
+  - swipl css-inline.pl swish-min.css
+
+Copyright: Public domain
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+:- use_module(library(dcg/basics)).
+:- use_module(library(pure_input)).
+:- use_module(library(readutil)).
+:- use_module(library(apply)).
+:- use_module(library(main)).
+:- use_module(library(debug)).
+
+:- initialization main.
+
+main([In, Out]) :-
+	process(In, Out).
+main([In]) :-
+	process(In, In).
+
+process(In, Out) :-
+	phrase_from_file(css(Data), In),
+	setup_call_cleanup(
+	    open(Out, write, O),
+	    maplist(save(O), Data),
+	    close(O)).
+
+css([s(S), url(URL)|Rest]) -->
+	string(Codes),
+	"url(", !, string(URLCodes), ")", !,
+	{ string_codes(S, Codes),
+	  atom_codes(URL, URLCodes)
+	},
+	css(Rest).
+css([s(S)]) -->
+	string(Codes), \+ [_], !,
+	{ string_codes(S, Codes) }.
+
+save(Out, s(S)) :- !,
+	format(Out, '~s', [S]).
+save(Out, url(S)) :-
+	\+ sub_string(S, 0, _, _, 'data:'),
+	once(sub_string(S, _, _, _, '/bower_components/')), % TBD: parameter
+	file_name_extension(_, Ext, S),
+	image_ext(Ext), !,
+	size_file(S, Size),
+	debug(css, 'Inlining image: ~q (~D bytes)~n', [S, Size]),
+	read_file_to_codes(S, Codes, [type(binary)]),
+	phrase(base64(Codes), Base64),
+	format(Out, 'url(data:image/~w;base64,~s)', [Ext,Base64]).
+save(Out, url(S)) :-
+	debug(css, 'URL: ~q~n', [S]),
+	format(Out, 'url(~s)', [S]).
+
+image_ext(gif).
+image_ext(png).
