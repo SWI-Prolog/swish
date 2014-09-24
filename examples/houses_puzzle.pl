d929856720827c6b03af035b7f282f13d0bead15
Markus Triska <markus.triska@gmx.at>
2014-09-24 23:11:15 +0200

    follow the conventions of the other examples
    Lists are named with an "s" at the end, and a blank separates predicate
    arguments.

--- examples/houses_puzzle.pl
+++ examples/houses_puzzle.pl
@@ -26,25 +26,25 @@ Who owns the zebra and who drinks water?
 */
 
 
-houses(Solution) :-
+houses(Hs) :-
 	% each house is represented as: h(Nationality, Pet, Cigarette, Drink, Color)
-	length(Solution, 5),                                                %  1
-	member(h(english, _, _, _, red), Solution),                         %  2
-	member(h(spanish, dog, _, _, _), Solution),                         %  3
-	member(h(_, _, _, coffee, green), Solution),                        %  4
-	member(h(ukrainian, _, _, tea, _), Solution),                       %  5
-	next(h(_, _, _, _, green), h(_, _, _, _, white), Solution),         %  6
-	member(h(_, snake, winston, _, _), Solution),                       %  7
-	member(h(_, _, kool, _, yellow), Solution),                         %  8
-	Solution = [_, _, h(_, _, _, milk, _), _, _],                       %  9
-	Solution = [h(norwegian, _, _, _, _)| _],                           % 10
-	next(h(_, fox, _, _, _), h(_, _, chesterfield, _, _), Solution),    % 11
-	next(h(_, _, kool, _, _), h(_, horse, _, _, _), Solution),          % 12
-	member(h(_, _, lucky, juice, _), Solution),                         % 13
-	member(h(japonese, _, kent, _, _), Solution),                       % 14
-	next(h(norwegian, _, _, _, _), h(_, _, _, _, blue), Solution),      % 15
-	member(h(_, _, _, water, _), Solution),		% one of them drinks water
-	member(h(_, zebra, _, _, _), Solution).		% one of them owns a zebra
+	length(Hs, 5),                                            %  1
+	member(h(english,_,_,_,red), Hs),                         %  2
+	member(h(spanish,dog,_,_,_), Hs),                         %  3
+	member(h(_,_,_,coffee,green), Hs),                        %  4
+	member(h(ukrainian,_,_,tea,_), Hs),                       %  5
+	next(h(_,_,_,_, green), h(_,_,_,_, white), Hs),           %  6
+	member(h(_,snake,winston,_,_), Hs),                       %  7
+	member(h(_,_,kool,_,yellow), Hs),                         %  8
+	Hs = [_,_,h(_,_,_, milk,_),_,_],                          %  9
+	Hs = [h(norwegian,_,_,_,_)|_],                            % 10
+	next(h(_,fox,_,_,_), h(_,_,chesterfield,_,_), Hs),        % 11
+	next(h(_,_,kool,_,_), h(_,horse,_,_,_), Hs),              % 12
+	member(h(_,_,lucky,juice,_), Hs),                         % 13
+	member(h(japonese,_,kent,_,_), Hs),                       % 14
+	next(h(norwegian,_,_,_,_), h(_,_,_,_,blue), Hs),          % 15
+	member(h(_,_,_,water,_), Hs),		% one of them drinks water
+	member(h(_,zebra,_,_,_), Hs).		% one of them owns a zebra
 
 next(A, B, Ls) :-
         nth0(IA, Ls, A),
