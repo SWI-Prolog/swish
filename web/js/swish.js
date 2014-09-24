b22a7f1881648db54885ccc0933bb9f6a3738db1
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-09-24 16:15:26 +0200

    Raise RequireJS timeout as we serve a big file.

--- web/js/swish.js
+++ web/js/swish.js
@@ -9,6 +9,7 @@
  */
 
 require.config({
+  waitSeconds: 60,			/* swish-min.js is big */
   paths:
   { jquery:      "../bower_components/jquery/dist/jquery.min",
     "jquery-ui": "../bower_components/jquery-ui/jquery-ui.min",
