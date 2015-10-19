5969b8c616d330c28ff67b6cb4cb98ce2ac65061
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-10-19 11:56:35 +0200

    ADDED: several statistics.

--- lib/swish_debug.pl
+++ lib/swish_debug.pl
@@ -1,7 +1,10 @@
 :- module(swish_debug,
-	  [ pengine_stale_module/1	% -Module
+	  [ pengine_stale_module/1,	% -Module
+	    swish_statistics/1		% -Statistics
 	  ]).
 :- use_module(library(pengines)).
+:- use_module(library(broadcast)).
+:- use_module(highlight).
 
 %%	pengine_stale_module(-M) is nondet.
 %
@@ -11,12 +14,36 @@
 pengine_stale_module(M) :-
 	current_module(M),
 	is_uuid(M),
-	\+ live_module(M).
+	\+ live_module(M),
+	\+ current_highlight_state(M, _).
 
 live_module(M) :-
 	pengine_property(Pengine, module(M)),
 	pengine_property(Pengine, thread(Thread)),
 	catch(thread_property(Thread, status(running)), _, fail).
+
+%%	swish_statistics(?State)
+%
+%	True if State is a statistics about SWISH
+
+swish_statistics(highlight_states(Count)) :-
+	aggregate_all(count, current_highlight_state(_,_), Count).
+swish_statistics(pengines_created(Count)) :-
+	(   flag(pengines_created, Old, Old)
+	->  Count = Old
+	;   Count = 0
+	).
+
+:- listen(pengine(Action), swish_update_stats(Action)).
+
+swish_update_stats(create(_Pengine, _Application, _Options0)) :-
+	flag(pengines_created, Old, Old+1).
+swish_update_stats(send(_Pengine, _Event)).
+
+
+%%	is_uuid(@UUID)
+%
+%	True if UUID looks like a UUID
 
 is_uuid(M) :-
 	atom(M),
@@ -36,3 +63,4 @@ uuid_code(_, X) :- char_type(X, xdigit(_)).
 	sandbox:safe_primitive/1.
 
 sandbox:safe_primitive(swish_debug:pengine_stale_module(_)).
+sandbox:safe_primitive(swish_debug:swish_statistics(_)).
