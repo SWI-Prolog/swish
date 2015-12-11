a19f58b88f7d83c070d6d4d3ce8376044f47a740
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-12-11 14:45:37 +0100

    FIXED: CHR programs cannot run in CHR debug mode.  Set CHR debug to off.

--- swish.pl
+++ swish.pl
@@ -158,7 +158,7 @@ pengines:prepare_module(Module, swish, _Options) :-
 
 :- use_module(library(clpfd), []).
 :- use_module(library(clpb), []).
-:- use_module(library(chr), []).
+:- use_module(lib/chr, []).
 
 % load rendering modules
 
