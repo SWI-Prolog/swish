7047c861f5b183b68f30c7772772eaa183306379
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2015-12-20 19:30:09 +0100

    coin ex

--- examples/coin.pl
+++ examples/coin.pl
@@ -4,6 +4,8 @@ J. Vennekens, S. Verbaeten, and M. Bruynooghe. Logic programs with annotated
 disjunctions. In International Conference on Logic Programming, 
 volume 3131 of LNCS, pages 195-209. Springer, 2004.
 */
+:- use_module(library(pita)).
+:- cplint.
 
 heads(Coin): 1/2; tails(Coin) : 1/2:-toss(Coin),\+biased(Coin).
 % if we toss a Coin that is not biased then it lands heads with probability 1/2
@@ -15,11 +17,12 @@ fair(Coin):0.9 ; biased(Coin):0.1.
 % a Coin is fair with probability 0.9 and biased with probability 0.1
 toss(coin).
 % coin is certainly tossed
+:- end_cplint.
 
 /** <examples>
 
-?- heads(coin).  % what is the probability that coin lands heads?
-?- tails(coin).  % what is the probability that coin lands tails?
+?- prob(heads(coin),Prob).  % what is the probability that coin lands heads?
+?- prob(tails(coin),Prob).  % what is the probability that coin lands tails?
 
 
 */
