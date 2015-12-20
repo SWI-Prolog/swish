f5544c72f12f7f54e2587200e3b1f235d02788f7
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2015-12-20 21:41:31 +0100

    prolog examples

---
+++ examples/path.pl
@@ -0,0 +1,36 @@
+/*
+Computing the probability of a path between two nodes in a probabilistic graph.
+Each edge has a probability of being present.
+From
+L. De Raedt, A. Kimmig, and H. Toivonen. ProbLog: A probabilistic Prolog and 
+its application in link discovery. In International Joint Conference on 
+Artificial Intelligence, pages 2462-2467, 2007.
+*/
+:- use_module(library(pita)).
+:- cplint.
+
+% path(X,Y) is true if there is a path between nodes X and Y
+% edge(a,b) indicates that there is an edge between a nad b
+
+path(X,X).
+% there is surely a path between a node and itself
+
+path(X,Y):-
+  edge(X,Z),path(Z,Y).
+% there is surely a path between X and Y if there is another node Z such that
+% there is an edge between X and Z and there is a path between Z and Y
+
+edge(a,b):0.2.
+% there is an edge between a and b with probability 0.2
+edge(a,c):0.3.
+edge(c,d):0.4.
+edge(d,e):0.4.
+edge(a,e):0.1.
+
+:- end_cplint.
+
+/** <examples>
+
+?- prob(path(a,e),Prob). % what is the probability that a and e are connected?
+
+*/
---
+++ examples/threesideddice.pl
@@ -0,0 +1,34 @@
+/*
+A three-sided die is repeatedly thrown until the outcome is three.
+on(T,F) means that on the Tth throw the face F came out.
+From
+Riguzzi and Terrance Swift. The PITA system: Tabling and answer subsumption for 
+reasoning under uncertainty. Theory and Practice of Logic Programming, 
+27th International Conference on Logic Programming (ICLP'11) Special Issue, 
+Lexington, Kentucky 6-10 July 2011, 11(4-5):433-449, 2011
+*/
+:- use_module(library(pita)).
+:- cplint.
+
+% on(T,F) means that the dice landed on face F at time T
+on(0,1):1/3;on(0,2):1/3;on(0,3):1/3.
+% at time 0 the dice lands on one of its faces with equal probability
+
+on(X,1):1/3;on(X,2):1/3;on(X,3):1/3:-
+  X1 is X-1,X1>=0,
+  on(X1,_),
+  \+ on(X1,3).
+% at time T the dice lands on one of its faces with equal probability if
+% at the previous time point it was thrown and it did not landed on face 3
+
+:- end_cplint.
+
+/** <examples>
+
+?- prob(on(0,1),Prob). % what is the probability that the dice lands on face 1 at time 0?
+?- prob(on(1,1),Prob). % what is the probability that the dice lands on face 1 at time 1?
+?- prob(on(2,1),Prob). % what is the probability that the dice lands on face 1 at time 2?
+?- prob(on(3,1),Prob). % what is the probability that the dice lands on face 1 at time 3?
+
+*/
+ 
