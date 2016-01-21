b8fb862add23232c4b8870d3fe426731cb34b1bd
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-01-21 10:24:23 +0100

    auc example

---
+++ examples/exauc.pl
@@ -0,0 +1,17 @@
+:-use_module(library(auc)).
+
+:- if(current_predicate(use_rendering/1)).
+:- use_rendering(c3).
+:- endif.
+
+
+main(AUCROC, ROC, AUCPR, PR):-
+  compute_areas_diagrams(
+    [0.7-a,0.7-a,0.7 - \+(a),0.6-a,0.6- \+(a),0.5- a,0.4- \+(a)],
+    4, 3, AUCROC, ROC, AUCPR, PR).
+
+/** <examples>
+
+?- main(AUCROC, ROC, AUCPR, PR). 
+
+*/
