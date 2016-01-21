90aa811435f033decdc2f73074a4de953b202ee5
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-01-21 10:32:45 +0100

    fixed exauc.pl

--- examples/exauc.pl
+++ examples/exauc.pl
@@ -8,10 +8,4 @@
 main(AUCROC, ROC, AUCPR, PR):-
   compute_areas_diagrams(
     [0.7-a,0.7-a,0.7 - \+(a),0.6-a,0.6- \+(a),0.5- a,0.4- \+(a)],
-    4, 3, AUCROC, ROC, AUCPR, PR).
-
-/** <examples>
-
-?- main(AUCROC, ROC, AUCPR, PR). 
-
-*/
+    AUCROC, ROC, AUCPR, PR).
