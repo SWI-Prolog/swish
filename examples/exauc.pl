a9d90fa088918829d9b7306435ea65f5744ce465
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-01-23 10:02:34 +0100

    exauc formatting

--- examples/exauc.pl
+++ examples/exauc.pl
@@ -9,7 +9,8 @@ main(AUCROC, ROC, AUCPR, PR):-
   compute_areas_diagrams(
     % list of couples prob-example where example is an atom for positive 
     % examples and \+(atom) for negative examples
-    [0.7 - a, 0.7 - a, 0.7 - \+(a), 0.6 - a, 0.6 - \+(a), 0.5 - a, 0.4 - \+(a)],
+    [0.7 - a, 0.7 - a, 0.7 - \+(a), 0.6 - a, 
+     0.6 - \+(a), 0.5 - a, 0.4 - \+(a)],
     AUCROC, ROC, AUCPR, PR).
 
 
