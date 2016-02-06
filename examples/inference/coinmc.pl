30f2024b5bddec61a264cfe0da47cfabee3712bb
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-02-06 22:13:38 +0100

    better query comments

--- examples/inference/coinmc.pl
+++ examples/inference/coinmc.pl
@@ -35,13 +35,27 @@ toss(coin).
 % expected result 0.51
 ?- mc_prob_bar(tails(coin),Prob).  % what is the probability that coin lands tails?
 % expected result 0.49
-?- mc_sample(heads(coin),1000,T,F,Prob).  % take 1000 sample of heads(coin)
-?- mc_sample(tails(coin),1000,T,F,Prob).  % take 1000 sample of tails(coin)
-?- mc_sample_bar(heads(coin),1000,Chart).  % take 1000 sample of heads(coin)
-?- mc_sample_bar(tails(coin),1000,Chart).  % take 1000 sample of tails(coin)
+?- mc_sample(heads(coin),1000,T,F,Prob).  
+% take 1000 sample of heads(coin) and return the number of successes (T),
+% the number of failures (F) and the probability
 
+?- mc_sample(tails(coin),1000,T,F,Prob).  
+% take 1000 sample of tails(coin) and return the number of successes (T),
+% the number of failures (F) and the probability
 
+?- mc_sample(heads(coin),1000,Prob).  
+% take 1000 sample of heads(coin) and return the probability
 
+?- mc_sample(tails(coin),1000,Prob).  
+% take 1000 sample of tails(coin) and return the probability
+
+?- mc_sample_bar(heads(coin),1000,Chart).  
+% take 1000 sample of heads(coin) and chart the number of successes and 
+% faliures
+
+?- mc_sample_bar(tails(coin),1000,Chart).  
+% take 1000 sample of tails(coin) and chart the number of successes and 
+% faliures
 
 
 */
