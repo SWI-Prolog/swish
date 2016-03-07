153e63886c439377097df5c7488e0d0005a2fe57
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-03-07 10:28:59 +0100

    FIXED: ?q=query option if the code contains examples.

--- web/js/jswish.js
+++ web/js/jswish.js
@@ -159,9 +159,8 @@ preferences.setDefault("emacs-keybinding", false);
 	    },
 	    examples: elem.swish('examples'),
 	    runner:   data.runner,
+	    editor:   editor[0]
 	  });
-
-	editor.prologEditor('makeCurrent');
 
 	$(".notebook").notebook();
 
--- web/js/query.js
+++ web/js/query.js
@@ -72,22 +72,27 @@ define([ "jquery", "config", "preferences", "cm/lib/codemirror",
 				}
 		              });
 
-	if ( typeof(data.examples) == "object" &&
-	     data.examples[0] &&
-	     !$(qediv).prologEditor('getSource', "query") )
-	  $(qediv).prologEditor('setSource', data.examples[0]);
+	elem.data(pluginName, data);
+
+	if ( !$(qediv).prologEditor('getSource', "query") )
+	{ if ( typeof(data.examples) == "object" ) {
+	    if ( data.examples[0] )
+	      $(qediv).prologEditor('setSource', data.examples[0]);
+	  } else {
+	    elem[pluginName]('setProgramEditor', $(data.editor), true);
+	  }
+	}
 
 	elem.on("current-program", function(ev, editor) {
 	  elem[pluginName]('setProgramEditor', $(editor));
 	});
 	elem.on("program-loaded", function(ev, editor) {
-	  if ( $(data.editor).data('prologEditor') == $(editor).data('prologEditor') ) {
+	  if ( $(data.editor).data('prologEditor') ==
+	       $(editor).data('prologEditor') ) {
 	    var exl = data.examples();
 	    elem.queryEditor('setQuery', exl && exl[0] ? exl[0] : "");
 	  }
 	});
-
-	elem.data(pluginName, data);
       });
     },
 
@@ -95,8 +100,11 @@ define([ "jquery", "config", "preferences", "cm/lib/codemirror",
      * @param {jQuery} editor has become the new current program
      * editor.  Update the examples and re-run the query highlighting.
      */
-    setProgramEditor: function(editor) {
+    setProgramEditor: function(editor, force) {
       var data = this.data(pluginName);
+
+      if ( data.editor == editor[0] && !force )
+	return this;
 
       data.editor = editor[0];
       if ( data.editor ) {
