2394852e1006a87388e58ad62302af35b6434a6c
Jan Wielemaker <j.wielemaker@cwi.nl>
2017-03-16 14:17:17 +0100

    ADDED: debug and logging config files.

---
+++ config-available/logging.pl
@@ -0,0 +1,56 @@
+/*  Part of SWISH
+
+    Author:        Jan Wielemaker
+    E-mail:        J.Wielemaker@cs.vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2017, VU University Amsterdam
+			 CWI Amsterdam
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in
+       the documentation and/or other materials provided with the
+       distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
+:- module(swish_config_logging, []).
+:- use_module(library(settings)).
+:- use_module(library(http/http_log)).
+:- use_module(lib/logging).
+:- use_module(library(debug)).
+
+/** <module> Configure logging facilities
+
+The  settings  below  enable  extensive  logging  of  HTTP  and  pengine
+interaction. The log files are rotated every  week and logs are kept for
+6 months. The log files can  be   used  together  with lib/replay,pl and
+lib/replay_cm.pl  to  replay   Pengine    interaction   and   CodeMirror
+highlighting interaction.
+*/
+
+:- debug_message_context(+time).
+:- set_setting_default(http:log_post_data, 1 000 000).
+:- set_setting_default(http:logfile, 'log/httpd.log').
+:- http_schedule_logrotate(weekly(sun, 05:05),
+                           [ keep_logs(26)
+                           ]).
