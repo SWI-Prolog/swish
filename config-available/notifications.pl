2cc6d27969b8fb5baf8df293ed11cabc623511b4
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-03-17 13:39:51 +0100

    Notifications require a profile and email to be configured.

--- config-available/notifications.pl
+++ config-available/notifications.pl
@@ -35,6 +35,8 @@
 
 :- module(config_notifications, []).
 :- use_module(swish(lib/plugin/notify), []).
+:- use_module(config(user_profile)).
+:- use_module(config_enabled(email)).
 
 /** <module> Configure notifications
 */
