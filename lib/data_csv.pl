2a6c5568802dd7dd6be210478b83d12bed776369
Jan Wielemaker <j.wielemaker@cwi.nl>
2017-03-21 10:47:02 +0100

    Fetch and records are working.

--- lib/data_csv.pl
+++ lib/data_csv.pl
@@ -34,13 +34,17 @@
 */
 
 :- module(data_csv,
-          [
+          [ csv_data/3,                 % :Id, +URL, +Options
+            record/2,                   % :Id, -Record
+
+            data_flush/1                % +Hash
           ]).
 :- use_module(library(csv)).
 :- use_module(library(option)).
 :- use_module(library(error)).
 :- use_module(library(lazy_lists)).
 :- use_module(library(apply)).
+:- use_module(library(date)).
 :- use_module(library(http/http_open)).
 
 /** <module> Cached CSV data access
@@ -52,7 +56,12 @@ and thus is is  stored  under  a   hash  constructed  from  the  URL and
 processing options.
 */
 
-%!  csv_data(+Id, +URL, +Options)
+:- meta_predicate
+    csv_data(:, +, +),
+    record(:, -).
+
+
+%!  csv_data(:Id, +URL, +Options) is det.
 %
 %   Make the CSV data in URL available   using  Id. Given this id, dicts
 %   with a tag Id are expanded to goals   on  the CSV data. In addition,
@@ -66,27 +75,87 @@ processing options.
 %       Names for the columns. If not provided, the first row is assumed
 %       to hold the column names.
 
+csv_data(M:Id, URL, Options) :-
+    variant_sha1(URL+Options, Hash),
+    data_source(Hash, import_csv(URL, Options)),
+    !,
+    (   clause(M:'$data'(Id, Hash), true)
+    ->  true
+    ;   assertz(M:'$data'(Id, Hash))
+    ).
+csv_data(M:Id, URL, Options) :-
+    variant_sha1(URL+Options, Hash),
+    assertz(data_source(Hash, import_csv(URL, Options))),
+    assertz(M:'$data'(Id, Hash)).
+
+%!  record(:Id, -Record) is nondet.
+%
+%   True when Record is a record in the dataset identified by Id.
+
+record(M:Id, Record) :-
+    clause(M:'$data'(Id, Hash), true),
+    !,
+    materialize(Hash),
+    data_signature(Hash, Signature),
+    data_record(Signature, Id, Record, Head),
+    call(Head).
+
+record(_:Id, _Record) :-
+    existence_error(dataset, Id).
+
+data_record(Signature, Tag, Record, Head) :-
+    Signature =.. [Name|Keys],
+    pairs_keys_values(Pairs, Keys, Values),
+    dict_pairs(Record, Tag, Pairs),
+    Head =.. [Name|Values].
+
+
+%!  materialize(+Hash)
+%
+%   Materialise the data identified by Hash
+
+materialize(Hash) :-
+    must_be(atom, Hash),
+    data_materialized(Hash, _When, _From),
+    !.
+materialize(Hash) :-
+    data_source(Hash, Goal),
+    call(Goal).
+
+
+%!  data_flush(+Hash)
+%
+%   Drop the data associated with hash
+
+data_flush(Hash) :-
+    data_signature(Hash, Signature),
+    data_record(Signature, _Id, _Record, Head),
+    retractall(Head),
+    retractall(data_signature(Hash, Head)),
+    retractall(data_materialized(Hash, _When1, _From)),
+    retractall(data_last_access(Hash, _When2)).
+
 
 		 /*******************************
-		 *            STORE		*
+		 *           CSV IMPORT		*
 		 *******************************/
 
 :- dynamic
-    data/3,             % Hash, URL, Options
-    data_columns/2,	% Hash, Columns
-    data_retrieved/3,	% Hash, Retrieved, LastModified
-    data_last_access/2. % Hash, Time
+    data_source/2,                      % Hash, Goal
+    data_signature/2,                   % Hash, Signature
+    data_materialized/3,                % Hash, Materialized, SourceID
+    data_last_access/2.                 % Hash, Time
 
-load_csv(URL, Options) :-
+import_csv(URL, Options) :-
     variant_sha1(URL+Options, Hash),
     setup_call_catcher_cleanup(
         http_open(URL, In,
                   [ header(content_type, ContentType),
-                    last_modified(LastModified)
+                    header(last_modified, LastModified)
                   ]),
         load_csv_stream(In, Hash, ContentType, Signature, Options),
         Catcher,
-        finalize(Catcher, In, LastModified, Signature)).
+        finalize(Catcher, In, LastModified, import_csv(URL, Options), Signature)).
 
 load_csv_stream(In, Hash, ContentType, Signature, Options) :-
     set_encoding(In, ContentType, Options),
@@ -109,7 +178,7 @@ signature([Head|Rows], Rows, _Hash, Signature, _Options) :-
     must_be(list(atom), Names),
     Signature = Head.
 
-finalize(exit, In, LastModified, Signature) :-
+finalize(exit, In, LastModified, _Action, Signature) :-
     !,
     close(In),
     functor(Signature, Hash, _),
@@ -118,10 +187,17 @@ finalize(exit, In, LastModified, Signature) :-
     ;   LastModStamp = (-)
     ),
     get_time(Now),
-    assertz(data_retrieved(Hash, Now, LastModStamp)).
-finalize(Reason, In, _LastModified, Signature) :-
+    assertz(data_materialized(Hash, Now, LastModStamp)),
+    assertz(data_signature(Hash, Signature)).
+finalize(Reason, In, _LastModified, Action, Signature) :-
     close(In),
-    retractall(Signature).
+    retractall(Signature),
+    failed(Reason, Action).
+
+failed(exception(Ex), _) :-
+    throw(Ex).
+failed(fail, Action) :-
+    throw(error(failure_error(Action))).
 
 
 %!  lazy_read_rows(+Stream, +Options, -List, -Tail)
@@ -136,3 +212,16 @@ lazy_read_rows(Stream, Options, List, Tail) :-
 :- lazy_list_iterator(lazy_read_rows(Stream, RecordOptions), Row,
                       csv_read_row(Stream, Row, RecordOptions),
                       Row == end_of_file).
+
+
+		 /*******************************
+		 *            SANDBOX		*
+		 *******************************/
+
+:- multifile
+    sandbox:safe_meta/2.
+
+sandbox:safe_meta(data_csv:csv_data(_:_,_,_), []) :- !, fail.
+sandbox:safe_meta(data_csv:record(_:_, _), []) :- !, fail.
+sandbox:safe_meta(data_csv:csv_data(_,_,_), []).
+sandbox:safe_meta(data_csv:record(_, _), []).
