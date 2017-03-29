1addee5bbbf35a77aa19f86897aec1cbee08094c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-03-29 11:51:19 +0200

    Document the HDT config file.

--- config-available/hdt.pl
+++ config-available/hdt.pl
@@ -36,7 +36,6 @@
 :- module(swish_config_hdt, []).
 :- use_module(library(broadcast)).
 :- use_module(swish(lib/plugin/hdt)).
-:- use_module(swish:swish(lib/plugin/hdt)).
 :- use_module(library(semweb/rdf11),
               [ rdf_register_prefix/2,
                 rdf_global_id/2,
@@ -44,14 +43,47 @@
               ]).
 :- use_module(swish(lib/render/rdf), []).
 
+/** <module> Make background RDF data available using HDT
+
+This config file makes possibly very large amounts of RDF data available
+from swish using the Header Dictionary Triple (HDT) format. Steps to use
+this:
+
+  1. Install an activate the HDT library using the commands below.  This
+     should work on Ubuntu out of the box.  On other Unix systems you
+     may need to tweak a little.  Installing on Windows is most likely
+     a challenge.
+
+     ```
+     sudo apt-get install libserd-dev libraptor-dev
+     git submodule update --init pack/hdt
+     swipl run.pl
+     ?- pack_rebuild(hdt).
+     ```
+
+  3. Create a directory with HDT files in them you want to make
+     available.  You can later add files without restarting SWISH.
+
+  4. Copy this file to `config-enabled` and go through the `EDIT`
+     marked sections to configure HDT for your data.
+*/
+
 :- multifile user:file_search_path/2.
 
+% EDIT: location of your HDT files
 user:file_search_path(hdt, '../hdt-data').
 
+% EDIT: uncomment to make hdt available without including the library
+% :- use_module(swish:swish(lib/plugin/hdt)).
+
+% EDIT: Define common RDF prefixes
 :- rdf_register_prefix(wgs84, 'http://www.w3.org/2003/01/geo/wgs84_pos#').
 
 :- rdf_meta(prefix(r, r)).
 
+% EDIT: define prefixes used in the various HDT datasets.  The RDF
+% rendering library will use to display the (english) label for
+% resources when available.
 rdf_prefix(hdt:geonames,      gn,    'http://www.geonames.org/').
 rdf_prefix(hdt:geonames,      gno,   'http://www.geonames.org/ontology#').
 rdf_prefix(hdt:linkedgeodata, lgo,   'http://linkedgeodata.org/ontology/').
@@ -65,10 +97,13 @@ register_prefixes :-
 
 :- listen(rdf_label(IRI, Label), label(IRI, Label)).
 
+% EDIT: Define how to obtain the label for an IRI. The default is to use
+% the rules above to find the HDT from   which the IRI originates and as
+% this HDT for rdfs:label in English.
 label(IRI, Label) :-
     rdf_global_id(Prefix:_Local, IRI),
     rdf_prefix(Graph, Prefix, _),
-    rdf(IRI, rdfs:label, Label@en, Graph).
+    rdf(IRI, rdfs:label, @(Label,en), Graph).
 
 :- initialization
     register_prefixes.
