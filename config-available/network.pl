5ac919be656b17925c4bf258097c7ad97cc4260f
Jan Wielemaker <j.wielemaker@cwi.nl>
2017-05-16 10:31:12 +0200

    Extended config-available/network.pl to indicate how to rebase the server.

--- config-available/network.pl
+++ config-available/network.pl
@@ -1,6 +1,7 @@
 :- module(config_swish_network, []).
 :- use_module(library(settings)).
 :- use_module(library(http/http_host)).
+:- use_module(library(http/http_path)).
 
 /* <module> Set HTTP access parameters
 
@@ -12,3 +13,14 @@ automatically.
 %:- set_setting_default(http:public_host,   localhost).
 %:- set_setting_default(http:public_port,   3050).
 %:- set_setting_default(http:public_scheme, http).
+
+:- multifile http:location/3.
+:- dynamic   http:location/3.
+
+% Rebase the server, such that the  home  is   not  on  /,  but (in this
+% example) on /swish/ This is needed  if   you  want to access the swish
+% server under a specific location using a  proxy. It *does not work* to
+% proxy /my-swish-app to http://machine.running.swish/
+
+http:location(root, '/swish', []).
+
