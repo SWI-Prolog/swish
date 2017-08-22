4c22e2b850518fc99830d3ba711ea0ddd3cf2351
Jan Wielemaker <j.wielemaker@cwi.nl>
2017-08-22 14:51:22 +0200

    FIXED: Call doc_enable/1 if provided to deal with PlDoc when loaded, but not enabled.

--- lib/template_hint.pl
+++ lib/template_hint.pl
@@ -43,6 +43,7 @@
 :- use_module(library(pldoc/doc_process)).
 :- use_module(library(pldoc/doc_wiki)).
 :- use_module(library(pldoc/doc_modes)).
+:- use_module(library(doc_http)).
 :- use_module(library(http/html_write)).
 :- use_module(library(memfile)).
 :- use_module(library(sgml)).
@@ -66,6 +67,10 @@ SWISH editor.
 @tbd	What about safely?
 @tbd	Dedicated template for the rendering support?
 */
+
+:- if(current_predicate(doc_enable/1)).
+:- initialization(doc_enable(true)).
+:- endif.
 
 %%	visible_predicate_templates(+Module, +Options, -Templates) is det.
 %
