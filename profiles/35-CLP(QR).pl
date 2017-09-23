fb5b4b8bb0df5111d5ba964b977172db47949950
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-09-23 15:26:31 +0200

    ADDED: CLP(QR) profile

---
+++ profiles/35-CLP(QR).pl
@@ -0,0 +1,12 @@
+% Constraint Logic Programming over rational and real numbers.
+% Both libraries define the predicate {}/1 and thus you can select
+% only one.
+:- use_module(library(clpq)).	% Constraints over Q (rational numbers)
+%:- use_module(library(clpr)).  % Constraints over R (real number)
+
+% Your program goes here
+
+
+/** <examples> Your example queries go here, e.g.
+?- { X = 2 * Y, Y = 1 - X }.
+*/
