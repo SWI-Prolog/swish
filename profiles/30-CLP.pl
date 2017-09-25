f3fcde3225492ac730846fc41fc785cae56efc6b
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-09-25 10:48:12 +0200

    Moved clp(q,r) support to examples.  Suggested by Markus Triska.

--- profiles/30-CLP.pl
+++ profiles/30-CLP.pl
@@ -1,9 +1,10 @@
 % Constraint Logic Programming
-:- use_module(library(dif)).		% Sound inequality
-:- use_module(library(clpfd)).		% Finite domain constraints
-:- use_module(library(clpb)).		% Boolean constraints
-:- use_module(library(chr)).		% Constraint Handling Rules
-:- use_module(library(when)).		% Coroutining
+:- use_module(library(dif)).	% Sound inequality
+:- use_module(library(clpfd)).	% Finite domain constraints
+:- use_module(library(clpb)).	% Boolean constraints
+:- use_module(library(chr)).	% Constraint Handling Rules
+:- use_module(library(when)).	% Coroutining
+%:- use_module(library(clpq)).  % Constraints over rational numbers
 
 % Your program goes here
 
