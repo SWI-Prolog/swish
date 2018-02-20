28c4cab178aa556938b82514b7770c3a158471fd
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-02-20 09:44:07 +0100

    Added feedback to use Ctrl+Enter.

--- web/js/codemirror/mode/prolog/prolog_query.js
+++ web/js/codemirror/mode/prolog/prolog_query.js
@@ -11,7 +11,34 @@
 })(function(CodeMirror) {
 "use strict";
 
+  CodeMirror.commands.prologMaybeFireQuery = function(cm) {
+    var lastl  = cm.lineCount()-1;
+
+    while(lastl >= 0 && cm.getLine(lastl).trim() == "")
+      lastl--;
+
+    if ( lastl >= 0 ) {
+      var endc  = cm.getLine(lastl).length;
+      var token = cm.getTokenAt({line:lastl, ch:endc}, true);
+
+      if ( token.type == "fullstop" ) {
+	var c = cm.getCursor();
+
+	if ( c.line > lastl || (c.line == lastl && c.ch >= token.end) ) {
+	  $(".swish-event-receiver")
+	     .trigger("feedback",
+		      { html: "Use <b>Ctrl+Enter</b> to execute the query",
+			owner: $(cm.display.wrapper).closest(".pane-wrapper")
+		      });
+	}
+      }
+    }
+
+    return CodeMirror.Pass;
+  }
+
   CodeMirror.commands.prologFireQuery = function(cm) {
+
     return cm.prologFireQuery(cm.getValue());
   }
 
@@ -20,7 +47,8 @@
       cm.removeKeyMap("prologQuery");
     if ( typeof(func) == "function" ) {
       var map = { name:         "prologQuery",
-		  "Ctrl-Enter": "prologFireQuery"
+		  "Ctrl-Enter": "prologFireQuery",
+		  "Enter":	"prologMaybeFireQuery"
 		};
       cm.addKeyMap(map);
       cm.prologFireQuery = func;
