6beb5161fbc9a6eba7f10c0c50ec566564eedca5
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-06-08 11:16:51 +0200

    Changes policy for thread scheduler.

--- lib/plugin/http_dyn_workers.pl
+++ lib/plugin/http_dyn_workers.pl
@@ -47,15 +47,22 @@
 /** <module> Dynamically schedule HTTP workers.
 
 This module defines  hooks  into  the   HTTP  framework  to  dynamically
-schedule worker threads.
+schedule worker threads. Dynamic scheduling relieves   us from finding a
+good value for the size of the HTTP worker pool.
 */
+
+%!  http:schedule_workers(+Dict)
+%
+%   Called if there  is  no  immediately   free  worker  to  handle  the
+%   incomming  request.  The  request  is    forwarded   to  the  thread
+%   =__http_scheduler= as the hook is called in time critical code.
 
 :- multifile
     http:schedule_workers/1.
 
 http:schedule_workers(Dict) :-
     get_time(Now),
-    catch(thread_send_message(http_scheduler, no_workers(Now, Dict)),
+    catch(thread_send_message('__http_scheduler', no_workers(Now, Dict)),
           error(existence_error(message_queue, _), _),
           fail),
     !.
@@ -65,7 +72,7 @@ http:schedule_workers(Dict) :-
 
 create_scheduler :-
     catch(thread_create(http_scheduler, _,
-                        [ alias(http_scheduler),
+                        [ alias('__http_scheduler'),
                           inherit_from(main),
                           debug(false),
                           detached(true)
@@ -90,14 +97,35 @@ http_scheduler(State) :-
     ;   http_scheduler(State)
     ).
 
-reschedule(no_workers(Reported, Dict), State0, State) :-
-    Backlog = Dict.waiting + State0.waiting*(0.5**((Reported-State0.time)/60.0)),
-    State = State0.put(_{waiting:Backlog, time:Reported}),
+%!  reschedule(+Message, +State0, -State) is semidet.
+
+reschedule(no_workers(Reported, Dict), State, State) :-
     aggregate_all(count, http_current_worker(Dict.port, _), Workers),
-    debug(http(scheduler), 'Waiting: ~w; accumulated: ~1f; active: ~w',
-          [Dict.waiting, Backlog, Workers]),
     setting(http:max_workers, MaxWorkers),
-    Backlog > MaxWorkers/(MaxWorkers-Workers),
-    http_add_worker(Dict.port,
-                    [ max_idle_time(10)
-                    ]).
+    Wait = 0.001*(MaxWorkers/(MaxWorkers-Workers)),
+    get_time(Now),
+    Sleep is Wait + Reported-Now,
+    debug(http(scheduler), 'Waiting: ~w; active: ~w; sleep: ~3f',
+          [Dict.waiting, Workers, Sleep]),
+    sleep(Sleep),
+    accept_queue(Dict, Queue),
+    message_queue_property(Queue, size(Newsize)),
+    (   Newsize == 0
+    ->  debug(http(scheduler), 'Drained', [])
+    ;   debug(http(scheduler), 'Size is ~w: adding worker', [Newsize]),
+        http_add_worker(Dict.port,
+                        [ max_idle_time(10)
+                        ])
+    ).
+
+%!  accept_queue(+Dict, -Queue)
+%
+%   As of 7.7.16, `queue` is a member   of  the provided dict. For older
+%   versions we need a hack.
+
+accept_queue(Dict, Queue) :-
+    Queue = Dict.get(queue),
+    !.
+accept_queue(Dict, Queue) :-
+    thread_httpd:current_server(Dict.port, _, _, Queue, _, _),
+    !.
