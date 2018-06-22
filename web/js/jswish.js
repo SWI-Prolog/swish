a1ec4c5dc279e62fc02e2085687e48db2dd00aaf
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-06-22 13:59:00 +0200

    ADDED: If `?restore=false` is added to the URL, tabs and the old content is not restored.

--- web/js/jswish.js
+++ web/js/jswish.js
@@ -263,6 +263,20 @@ preferences.setInform("preserve-state", ".unloadable");
       if ( preferences.getVal("preserve-state") == false )
 	return false;
 
+      function getQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+	  var pair = vars[i].split('=');
+	  if (decodeURIComponent(pair[0]) == variable) {
+	    return decodeURIComponent(pair[1]);
+	  }
+	}
+      }
+
+      if ( getQueryVariable("restore") == "false" )
+	return false;
+
       return true;
     },
 
