4c61916659e7b7eca479636aa2ccaca4cec743a3
Jan Wielemaker <J.Wielemaker@vu.nl>
2018-12-17 09:10:51 +0100

    Trying another approach at html output

--- lib/html_output.pl
+++ lib/html_output.pl
@@ -34,53 +34,122 @@
 */
 
 :- module(swish_html_output,
-          [ html/1,                     % +Spec
-            safe_html/1,                % +Spec
-            safe_html//1                % +Spec
+          [ html/1,                             % +Spec
+            html//1                             % +Spec, //
           ]).
-:- use_module(library(http/html_write)).
+:- use_module(library(http/html_write), except([html//1])).
+:- use_module(library(pengines)).
 :- use_module(library(sandbox)).
+:- use_module(library(lists)).
+:- use_module(library(error)).
 
+/** <module> SWISH HTML Output
+*/
 
-		 /*******************************
-		 *         OTHER OUTPUTS	*
-		 *******************************/
 
 %!  html(+Spec) is det.
 %
 %   Include HTML into the output.
 
 :- html_meta
-    html(html),
-    safe_html(html),
-    safe_html(html,?,?).
+    html(html).
 
 html(Spec) :-
+    make_safe_html(Spec, SafeSpec),
+    output_html(SafeSpec).
+
+html(Spec) -->
+    { make_safe_html(Spec, SafeSpec) },
+    html_write:html(SafeSpec).
+
+output_html(SafeSpec) :-
+    pengine_self(_),
+    !,
+    pengines_io:send_html(SafeSpec).
+output_html(Spec) :-
     phrase(html(div(Spec)), Tokens),
     with_output_to(
         string(HTML),
         print_html(current_output, Tokens)),
     format('~w', [HTML]).
 
-safe_html(Spec) :-
-    is_safe_html(Spec),
+make_safe_html(HTML0, HTML) :-
+    (   prolog_load_context(module, M)
+    ->  true
+    ;   pengine_self(M)
+    ),
+    make_safe_html(HTML0, M, HTML).
+
+make_safe_html(Var, M, swish_html_output:html(M:Var)) :-
+    var(Var),
+    !.
+make_safe_html(Module:HTML0, M, Module:HTML) :-
     !,
-    html(Spec).
-
-safe_html(Spec) -->
-    { is_safe_html(Spec) },
+    Module == M,
+    make_safe_html(HTML0, M, HTML).
+make_safe_html([], _, []) :-
+    !.
+make_safe_html([H0|T0], M, [H|T]) :-
     !,
-    html(Spec).
-
-is_safe_html(M:Spec) :-
-    prolog_load_context(module, M),
-    must_be(ground, Spec),
-    forall(sub_term(\(Eval), Spec),
-           safe_eval(Eval, M)).
-
-safe_eval(Goal, M) :-
+    make_safe_html(H0, M, H),
+    make_safe_html(T0, M, T).
+make_safe_html(Format-Args, _M, Format-Args) :-
+    !,
+    safe_goal(format(Format, Args)).
+make_safe_html(\Goal, M, \Goal) :-
+    !,
+    must_be(callable, Goal),
     dcg_extend(Goal, DcgGoal),
     safe_goal(M:DcgGoal).
+make_safe_html(Elem0, M, Elem) :-
+    Elem0 =.. [Name, Attrs0, Content0],
+    !,
+    safe_attrs(Attrs0, M, Attrs),
+    make_safe_html(Content0, M, Content),
+    Elem =.. [Name, Attrs, Content].
+make_safe_html(Elem0, M, Elem) :-
+    Elem0 =.. [Name, AttrsOrContent0],
+    !,
+    (   html_write:layout(Name, _, empty)
+    ->  safe_attrs(AttrsOrContent0, M, Safe)
+    ;   make_safe_html(AttrsOrContent0, M, Safe)
+    ),
+    Elem =.. [Name, Safe].
+make_safe_html(Text, _, Text) :-
+    atomic(Text).
+
+safe_attrs([], _, []) :-
+    !.
+safe_attrs([H0|T0], M, [H|T]) :-
+    !,
+    safe_attrs(H0, M, H),
+    safe_attrs(T0, M, T).
+safe_attrs(Name=Value0, M, Name=Value) :-
+    !,
+    safe_attr_value(Value0, M, Value).
+safe_attrs(NameValue0, M, NameValue) :-
+    !,
+    NameValue0 =.. [Name,Value0],
+    safe_attr_value(M, Value0, Value),
+    NameValue =.. [Name,Value].
+
+safe_attr_value(_, Value0, _) :-
+    var(Value0),
+    !,
+    instantiation_error(Value0).
+safe_attr_value(M, Value0, Value) :-
+    is_list(Value0),
+    !,
+    maplist(safe_attr_value(M), Value0, Value).
+safe_attr_value(_M, Format-Args, Format-Args) :-
+    !,
+    safe_goal(format(Format, Args)).
+safe_attr_value(M, A0+B0, A+B) :-
+    safe_attr_value(M, A0, A),
+    safe_attr_value(M, B0, B).
+safe_attr_value(_, V, V) :-
+    atomic(V).
+
 
 dcg_extend(Goal, DcgGoal) :-
     must_be(callable, Goal),
@@ -89,13 +158,7 @@ dcg_extend(Goal, DcgGoal) :-
     append(List, [_,_], ExList),
     DcgGoal =.. ExList.
 
-swish:goal_expansion(html(Spec), safe_html(Spec)).
-swish:goal_expansion(html(Spec, L,T), safe_html(Spec, L, T)).
-
 :- multifile sandbox:safe_primitive/1.
 
-sandbox:safe_meta_predicate(md_eval:safe_html/1).
-sandbox:safe_meta_predicate(md_eval:safe_html/3).
-sandbox:safe_primitive(md_eval:html(Spec)) :-
-    \+ sub_term(\(_), Spec).
-sandbox:safe_meta_predicate(md_eval:is_safe_html/1).
+sandbox:safe_meta_predicate(swish_html_output:html/1).
+sandbox:safe_meta_predicate(swish_html_output:html/3).
