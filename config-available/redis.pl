033a966b32d0322f72a5a43e844ad0008a6e9449
Jan Wielemaker <J.Wielemaker@vu.nl>
2020-10-01 14:05:59 +0200

    Config file for using redis.

---
+++ config-available/redis.pl
@@ -0,0 +1,10 @@
+:- module(config_redis, []).
+:- use_module(swish(lib/config), []).
+
+:- use_module(library(redis)).
+
+:- redis_server(swish, localhost:6379, []).
+
+:- multifile swish_config:config/2.
+swish_config:config(redis, swish).
+swish_config:config(redis_prefix, swish).
